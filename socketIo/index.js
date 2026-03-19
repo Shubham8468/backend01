981fdd1f4db2941eccabc03c014b65cdbc4ff3c1
unknown <shubh386a@gmail.com>
2026-03-20 04:16:46 +0530

    add socket.io

--- socketIo/index.js
+++ socketIo/index.js
@@ -5,7 +5,7 @@ import path from 'path'
 import {Server} from 'socket.io'
 const app=express();
 dotenv.config();
-const server=http.createServer(express)
+const server=http.createServer(app)
 const io= new Server(server);
 
 
@@ -15,11 +15,18 @@ const io= new Server(server);
 app.use(express.static(path.resolve('./public'))) // this is the method for not give 
 
 const port= process.env.PORT ||4000;
-app.get('/',(req,resp)=>{
-    
-   resp.sendFile(path.join('/index.html'))
-    
+//io connect
+io.on('connection',(socket)=>{
+    socket.on('msgFromFronted',(msg)=>{
+     console.log(msg)//msg recived from Frondend
+     //Now i send msg to frondend from Backend
+     io.emit("mesFromBackend",`${socket.id} : ${msg}`)
+    })
+    console.log( `a user connected :${socket.id}`)//jb bhi connect hoga to user id 
 })
-app.listen(port,()=>{
+
+
+
+server.listen(port,()=>{
     console.log(`server run on PORT : ${port}`)
 })
